b47bb5693bb001fae90d717e522717739c7a8a10
Jason Ericson <jason.l.ericson@gmail.com>
2024-11-20 20:46:04 -0600

    Corrected test and added custom plugin.

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -17,7 +17,7 @@ public class HelloWorldTest {
     public void testSayHello() {
         HelloWorld hw = new HelloWorld();
         hw.sayHello();
-        Assertions.assertEquals("Hello World", outStream.toString());
+        Assertions.assertEquals("Hello World.", outStream.toString());
     }
 
     @AfterEach
